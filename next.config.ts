30c578e56ed1c96cb66ccc63f88e2aeb6f919bf8
Akshit Talwar <akshittalwar@protonmail.com>
2025-04-27 16:21:52 +0530

    unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co', // Add other domains if needed
-        port: '',
-        pathname: '/**',
-      },
-      // Example for another domain:
       // {
       //   protocol: 'https',
       //   hostname: 'covers.example.com',
